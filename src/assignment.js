d1a6faadaed867f05af735ed494861233439cadb
Temitope Oloidi <108740822+temitope94@users.noreply.github.com>
2022-07-05 13:43:25 +0000

    attempt 1-3

--- src/assignment.js
+++ src/assignment.js
@@ -13,10 +13,13 @@ const assignment = {};
  * @param {Array} arrayOfNumbers the array of numbers to sum
  * @returns number the sum of the numbers
  */
-function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+ function sumOfNumbers(arrayOfNumbers) {
+    let arraySum = arrayOfNumbers.reduce((previousVal, currentVal) => {
+      return previousVal + currentVal;
+    });
+    return arraySum;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +31,10 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+		let result = arrayOfNumbers.filter(even => even % 2 == 0);
+    return result.length;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
@@ -47,9 +51,14 @@ function countEvenNumbers(arrayOfNumbers) {
  * @returns Array the converted temperatures in Fahrenheit
  */
 function celsiusToFahrenheit(arrayOfNumbers) {
-    return arrayOfNumbers;
+    let arr = [];
+    arrayOfNumbers.forEach( (cel) => {
+      let result = Math.trunc(cel * 9 / 5 + 32);
+      return arr.push(result);  
+    });
+    return arr;
 }
-// assignment.celsiusToFahrenheit = celsiusToFahrenheit;
+assignment.celsiusToFahrenheit = celsiusToFahrenheit;
 
 
 // ========================
